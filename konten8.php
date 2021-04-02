72e7c63a4539bd03a448a021b5b155607e1998a9
restufauzi1 <restuf443@gmail.com>
2021-04-02 21:28:17 +0700

    sandi

---
+++ konten8.php
@@ -0,0 +1,167 @@
+
+
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-danger sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3"> Admin <sup></sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>FAQ</span></a>
+            </li>
+
+            <!-- Divider -->
+           
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+            <!-- Sidebar Message -->
+    
+
+        </ul>
+        <!-- End of Sidebar -->
+ <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!-- Topbar Search -->
+               
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                        <li class="nav-item dropdown no-arrow d-sm-none">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-search fa-fw"></i>
+                            </a>
+                           
+                         
+
+                        <div class="topbar-divider d-none d-sm-block"></div>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                               </span>
+                                <img class="img-profile rounded-circle"
+                                    src="img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Activity Log
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="logout.php" >
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+  <!-- Begin Page Content -->
+              <div class="container-fluid">
+                  <div class="content">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header mb-5">
+                <h5 class="card-category"> FAQ APLIKASI SPP MAHARDHIKA</h5>
+                <h3 class="card-title">Penjelasannya</h3>
+              </div>
+              <div class="card-body">
+                <div class="typography-line">
+                  <p><span>APA ITU APLIKASI SPP MAHARDHIKA</span>
+                  aplikasi yang digunakan untuk kepentingan pembayaran uang spp dan kegiatan siswa bagi sekolah sekolah baik negri maupun swasta.
+                  </p>
+                </div>
+                <div class="typography-line">
+                  <span>Mengapa Harus Menggunakan Aplikasi Ini?</span>
+                  <blockquote>
+                    <p class="blockquote blockquote-primary">
+                      "penggunaan aplikasi pembayaran sekolah dapat memudahkan sekolah dalam mengingatkan tagihan biaya pendidikan pada orang tua/wali siswa"
+                      <br>
+                      <br>
+                      <small>
+                        - Mahardhika
+                      </small>
+                    </p>
+                  </blockquote>
+                </div>
+                <div class="typography-line">
+                  <span>Terdapat Fitur Apa Saja Sih?</span>
+                  <blockquote>
+                    <p class="blockquote blockquote-info">
+                      "Aplikasi SPP Mahardhika terdapat fitur-fitur khusus sesuai level Seperti : Admin (bisa melihat,menambah,mengubah,dan mengahpus data spp,kelas,pengguna,siswa,dan transaksi pembayaran. Serta Petugas (hanya bisa melihat,menambah,mengubah,dan menghapus transaksi pembayaran saja. dan terakhir Siswa (hanya bisa melihat history pembayaran saja."
+                      <br> 
+                      <br>
+                      <small>
+                        - Mahardhika
+                      </small>
+                    </p>
+                  </blockquote>
+                </div>
+                 <div class="typography-line">
+                  <span>Lah Kalau Begitu Orang Luar Bisa Mengakses?</span>
+                  <blockquote>
+                    <p class="blockquote blockquote-danger">
+                      "Tentu saja bisa, Cuman hanya login sebagai siswa. jadi orang tua pun tahu apakah anaknya membayar atau tidak?. dan cara mengaksesnya hanya cukup dengan mencari NISN siswa, di kolom pencaharian."
+                      <br>
+                      <br>
+                     
+                    </p>
+                  </blockquote>
+                </div>
+              </div>
+              </div>
+                    
